d46e2ec0796f148fdc6d397f26fb7f1ca53b62e8
Tommaso Scudella <tommaso.scudella08@gmail.com>
2026-03-16 16:52:01 +0100

    Aggiunto il main

--- Albero-binario/Program.cs
+++ Albero-binario/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace Albero_binario
+﻿using System;
+using System.Collections.Generic;
+using Albero_binario;
+
+class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Ciao Mondo!");
-        }
+        //crea un albero di interi
+        BinaryTree<int> albero = new BinaryTree<int>();
+        //imposta la radice
+        albero.SetRadice(10);
+        Node<int> radice = albero.GetRadice();
+        //aggiungi nodi
+        Node<int> nodo1 = albero.AggiungiNodo(radice, 5);  //figlio sinistro
+        Node<int> nodo2 = albero.AggiungiNodo(radice, 15); //figlio destro
+        //aggiungi sottoalbero
+        BinaryTree<int> sottoalbero = new BinaryTree<int>();
+        sottoalbero.SetRadice(20);
+        sottoalbero.AggiungiNodo(sottoalbero.GetRadice(), 25);
+        albero.AggiungiSottoalbero(sottoalbero, nodo1); //collega sottoalbero a nodo1
+        Console.WriteLine("numero totale di nodi: " + albero.NumNodi());
+        Console.WriteLine("grado della radice: " + albero.Grado(radice));
+        Console.WriteLine("padre di nodo 25: " + albero.Padre(sottoalbero.GetRadice()).Value);
+        //rimuovi un sottoalbero
+        BinaryTree<int> staccato = albero.RimuoviSottoalbero(nodo1);
+        Console.WriteLine("numero nodi nell'albero principale dopo rimozione: " + albero.NumNodi());
+        Console.WriteLine("numero nodi nel sottoalbero staccato: " + staccato.NumNodi());
     }
 }
